5a224634c832cabc88c4768080eae47fbed00ed5
DeazStar <deadfromtheborn@gmail.com>
2023-01-11 08:11:03 -0700

    fix

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,10 @@
-# Create a file in /tmp using Puppet
+#Create a file in /tmp
 
-file { 'holberton':
-  ensure  => present,
-  path    => '/tmp/holberton',
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0744',
-  content => 'I love Puppet',
+file { 'school':
+ensure  => 'present',
+path    => '/tmp/school',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet'
 }
